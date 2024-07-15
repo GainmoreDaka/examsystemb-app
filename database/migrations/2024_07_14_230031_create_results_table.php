f0afcd1697f658059e9459725d7400ae4bf7bb16
Gainmore Daka <elhaanan@gmail.com>
2024-07-15 22:36:23 +0100

    2nd Commit

--- database/migrations/2024_07_14_230031_create_results_table.php
+++ database/migrations/2024_07_14_230031_create_results_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('results', function (Blueprint $table) {
             $table->id();
             $table->foreignId('exam_id')->constrained();
+            $table->foreignId('student_id')->constrained('users');
+        
             $table->timestamps();
         });
     }
